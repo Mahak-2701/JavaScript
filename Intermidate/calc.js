63c15ce7c9de1e891e22bd1720dd1c2cc1b3ac98
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-08-16 21:45:54 +0530

    $$

---
+++ Intermidate/calc.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    if (b === 0) {
+      return "Cannot divide by zero";
+    }
+    return a / b;
+  }
+  
+  const num1 = 10;
+  const num2 = 5;
+  
+  console.log(`${num1} + ${num2} = ${add(num1, num2)}`);
+  console.log(`${num1} - ${num2} = ${subtract(num1, num2)}`);
+  console.log(`${num1} * ${num2} = ${multiply(num1, num2)}`);
+  console.log(`${num1} / ${num2} = ${divide(num1, num2)}`);
+
+  // easy
