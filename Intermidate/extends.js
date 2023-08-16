28dde77f7cf559b4e5d0248b66320bb98fd5b569
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-08-16 21:38:56 +0530

    $

---
+++ Intermidate/extends.js
@@ -0,0 +1,22 @@
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a sound.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    speak() {
+      console.log(`${this.name} barks.`);
+    }
+  }
+  
+  class Cat extends Animal {
+    speak() {
+      console.log(`${this.name} meows.`);
+    }
+  }
+  
