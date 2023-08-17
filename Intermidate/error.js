9f6450522496970bf4fc8ffcde9e2e551203938f
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-08-17 21:17:47 +0530

    .

--- Intermidate/error.js
+++ Intermidate/error.js
@@ -1,17 +1,16 @@
 
 // Run this plz
 function divide(a, b) {
-    if (b === 0) {
-      throw new Error("Division by zero is not allowed");
-    }
-    return a / b;
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
   }
-  
-  try {
-    const result = divide(10, 0);
-    console.log("Result:", result);
-  } catch (error) {
-    console.error("Caught an error:", error.message);
-    throw new Error("Caught error, throwing new error");
-  }
-  
+  return a / b;
+}
+
+try {
+  const result = divide(10, 0);
+  console.log("Result:", result);
+} catch (error) {
+  console.error("Caught an error:", error.message);
+  throw new Error("Caught error, throwing new error");
+}
