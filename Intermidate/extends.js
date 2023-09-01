235c053188a001dc73aeb12ce869e29ee1ca8062
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-01 19:06:49 +0530

    ~~

--- Intermidate/extends.js
+++ Intermidate/extends.js
@@ -1,42 +1,42 @@
 class Animal {
-    constructor(name) {
-      this.name = name;
-    }
-  
-    speak() {
-      console.log(`${this.name} makes a sound.`);
-    }
-    rest(){
-
-    }
-  }
-  
-  class Dog extends Animal {
-    speak() {
-      console.log(`${this.name} barks.`);
-    }
-    rest(){
-      
-    }
-  }
-  
-  class Cat extends Animal {
-    speak() {
-      console.log(`${this.name} meows.`);
-    }
-    rest(){
-      
-    }
+  constructor(name) {
+    this.name = name;
   }
 
-  class Koyal extends Animal, Cat{
-    fly(){
-      console.log( `{this.name} flys.`);
-    }
-    rest(){
-      
-    }
+  speak() {
+    console.log(`${this.name} makes a sound.`);
   }
+  rest() {
+
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(`${this.name} barks.`);
+  }
+  rest() {
+
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log(`${this.name} meows.`);
+  }
+  rest() {
+
+  }
+}
+
+class Koyal extends Animal, Cat {
+  fly() {
+    console.log(`{this.name} flys.`);
+  }
+  rest() {
+
+  }
+}
 // Execution
 const dog = new Dog("Buddy");
 const cat = new Cat("Whiskers");
