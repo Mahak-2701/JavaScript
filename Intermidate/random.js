d5d4247fb6549102411ad15b9999369b6b1c8754
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-07 19:09:36 +0530

    @

--- Intermidate/random.js
+++ Intermidate/random.js
@@ -6,3 +6,8 @@ console.log(guess);
 
 //guess = randomNumber; //easy
 
+if (guess === randomNumber) {
+    console.log("Congratulations! You guessed the correct number!");
+} else {
+    console.log("Sorry, that's incorrect. The number was " + randomNumber);
+}
