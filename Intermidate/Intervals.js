baadbfa642a5f45b279042f2a4435f108bc35b46
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-07 19:11:52 +0530

    ~~~

---
+++ Intermidate/Intervals.js
@@ -0,0 +1,13 @@
+// setTimeout and setInterval() functions
+
+// Use Case 1: setTimeout for Delayed Execution
+console.log("Use Case 1: setTimeout for Delayed Execution");
+
+function delayedFunction() {
+    console.log("This function was executed after a delay of 2 seconds.");
+}
+
+setTimeout(delayedFunction, 2000);
+
+// Use Case 2: setInterval for Repeated Execution
+console.log("\nUse Case 2: setInterval for Repeated Execution");
