78ec6b43c80270b199548ad2893dc7aba14b06b1
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-08 16:53:10 +0530

    ~

--- Advanced/first.js
+++ Advanced/first.js
@@ -1,12 +1,15 @@
-let arr = []
+async function main(){
+    let arr = []
 
-for(let i=0; i<=199; ++i){
-    const val = Math.floor(Math.random() * i)
-    setTimeout(() => {
-        console.log(`Pushing ${val} into Array.`);
-        arr.push(val);
-    }, 1000);
-    
+    for(let i=0; i<=199; ++i){
+        const val = Math.floor(Math.random() * i)
+        setTimeout(() => {
+            console.log(`Pushing ${val} into Array.`);
+            arr.push(val);
+        }, 1000);
+
+    }
+    console.log(`Displaying Data...`);
+    console.table(arr);
 }
-
-console.table(arr);
+main();
