addf52bbb001b7d3261c22e7bde6b445d5dd8c54
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-20 21:07:14 +0530

    Done

--- Advanced/Buffer.js
+++ Advanced/Buffer.js
@@ -3,7 +3,8 @@ const Buffer = require('buffer').Buffer;
 var _Name = Buffer.from("Mahak");
 var arr = [55,62,17,86,88];
 
-var Buff = Buffer.alloc(1024).from(arr);
+var Buff = Buffer.alloc(1024);
+Buff.write('Mahak v2');
 
 
 console.log(`Length of Name Buffer : ${_Name.length}`);
