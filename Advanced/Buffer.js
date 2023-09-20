a6e467098081adbcc7f0a526d7f180adf85c8885
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-20 21:00:54 +0530

    ~

--- Advanced/Buffer.js
+++ Advanced/Buffer.js
@@ -4,7 +4,7 @@ var _Name = Buffer.from("Mahak");
 var arr = [55,62,17,86,88];
 
 var Buff = Buffer.alloc(1024);
-
+Buff.write(arr);
 
 console.log(`Length of Name Buffer : ${_Name.length}`);
 console.log(`Buffered Data : ${_Name.toString('utf-8')}`);
