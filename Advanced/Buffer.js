89a0c2d7ef37b2fe9f30a5f18c29564029eab185
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-20 20:51:20 +0530

    Blah

--- Advanced/Buffer.js
+++ Advanced/Buffer.js
@@ -3,3 +3,4 @@ const Buffer = require('buffer').Buffer;
 var _Name = Buffer.from(['M', 'a', 'h', 'a', 'k']);
 
 console.log(`Length of Name Buffer : ${_Name.length}`);
+console.log(`Buffered Data : ${_Name.toString('utf-8')}`);
