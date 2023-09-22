d95563a2100d36ce94013d91e058b74ccbdc74f8
Mahak <sainimahak004@gmail.com>
2023-09-22 20:34:52 +0530

    Update timeout.js

--- Basic/timeout.js
+++ Basic/timeout.js
@@ -1,3 +1,3 @@
 console.log("Program Started");
 setTimeout(tim=()=>{console.log("Program terminated in 2.5 sec"),2000})
-console.log("Program terminated....");
+setTimeout(`console.log("Program Terminated.....)`,3000)
