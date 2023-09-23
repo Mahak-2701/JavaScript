58e0587669aca60591dbe3efd832008146c1d23a
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-09-23 20:31:58 +0530

    new

--- Advanced/arguments.js
+++ Advanced/arguments.js
@@ -1,1 +1,10 @@
 // arguments is an array-like object accessible inside functions that contains the values of the arguments passed to that function.
+function args(a, b, c, ...extra) {  
+    console.log(`-> Total Possible Indices :[${arguments.length - 1}]`);
+    console.log(arguments[0]); // a
+    console.log(arguments[1]); // b
+    console.log(arguments[2]); // c
+    console.log("Function Terminated\n");
+    
+    return a + b + c;
+}
