2f7d0a0bd0d031df97ceefb8c3229ca9a9afda1e
Mahak <sainimahak004@gmail.com>
2023-09-27 10:38:27 +0530

    Update server.js

--- Intermidate/server.js
+++ Intermidate/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer(function (res, req) {
     req.write("<b>hello</b><br>");
-    req.write("<span 'style= color:'green''></span>")
+    req.write("<span 'style= color:'green''> user~~~</span>")
     req.end();
 }).listen(8080);
