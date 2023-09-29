df8ca89705f165ede8d95ec5ff8e9bb7456b4e17
Mahak <sainimahak004@gmail.com>
2023-09-29 22:14:02 +0530

    Update fsmodule.js

--- Intermidate/fsmodule.js
+++ Intermidate/fsmodule.js
@@ -1,9 +1,4 @@
 
-const http = require('http');
-http.createServer(function (res, req) {
-    req.write("<b>hello</b><br>");
-    req.write("<span style= 'color:green';> user~~~</span>");
+
     const fs=require("fs");
     fs.readFile('server.js');
-    req.end();
-}).listen(8080);
