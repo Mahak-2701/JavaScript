0e6b8107fb76ed78e1c5f56ffbf58e7e38a2b494
Mahak <sainimahak004@gmail.com>
2023-10-12 14:43:55 +0530

    Update readfile.js

--- Intermidate/readfile.js
+++ Intermidate/readfile.js
@@ -1,6 +1,12 @@
 //asynchronized method/non-blocking
 const fs=require('fs');
-fs.readFile("./maha.js","utf-8",function(err,data){
+fs.readFile("./mahak.js","utf-8",function(err,data){
+    if(err) throw err;
+    else
+    console.log(data);
+})
+const fs=require('fs');
+fs.readFile("./mahak.js","utf-8",function(err,data){
     if(err) throw err;
     else
     console.log(data);
