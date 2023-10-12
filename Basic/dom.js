cddbf932c2c798502d5fc0a1bc1dedf06d0c853c
Mahak <sainimahak004@gmail.com>
2023-10-12 14:36:03 +0530

    Create dom.js

---
+++ Basic/dom.js
@@ -0,0 +1,8 @@
+for(var i=0;i<=5;i++){
+    var div=document.createElement("div");//create a div 
+    var h1=document.createElement("h1"); //create h1 in div
+    h1.innerHTML="this is h1";
+    div.appendChild(h1);//append h1 in  
+    h1.style.color="red";
+    document.body.appendChild(div);
+}
