c4d9562037bf64fcb1169c77c4fdcd88a39c78d4
Mahak <sainimahak004@gmail.com>
2023-10-13 20:21:55 +0530

    Update append.js

--- Intermidate/append.js
+++ Intermidate/append.js
@@ -1,7 +1,14 @@
 //append add data not overlaping
+// const fs=require('fs');
+// var content="hello by user!!";
+// fs.appendFile("demo.txt",content,function(error){
+//     if(error) throw errorMonitor;
+//     else console.log("Operation succesfully done!!");
+// })
+
+//synchronization
 const fs=require('fs');
 var content="hello by user!!";
-fs.appendFile("demo.txt",content,function(error){
-    if(error) throw errorMonitor;
-    else console.log("Operation succesfully done!!");
-})
+fs.appendFileSync("demo.txt",content);
+console.log(content);
+console.log("i am waiting!!");
