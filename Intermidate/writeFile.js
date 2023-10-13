a4b0a78504340b155a8cf21d27e253c6cf212368
Mahak <sainimahak004@gmail.com>
2023-10-13 20:16:39 +0530

    Update writeFile.js

--- Intermidate/writeFile.js
+++ Intermidate/writeFile.js
@@ -1,7 +1,18 @@
 //asynchronization write file
-const fs=require('fs');
+/*const fs=require('fs');
 var content="hello by admin!!";
 fs.writeFile("demo.txt",content,function(error){
     if(error) throw errorMonitor;
     else console.log("Operation succesfully done!!");
-})
+})*/
+
+//synchronization method
+const fs = require('fs');
+try {
+    var content = "hello by admin!!";
+    var data = fs.writeFileSync("demo.txt", content);
+}
+catch{
+    if(err)throw err;
+}
+
