544fc84d98023a981bd755e9c008b47952844811
Mahak <sainimahak004@gmail.com>
2023-10-18 22:09:05 +0530

    Update server.js

--- Intermidate/server.js
+++ Intermidate/server.js
@@ -6,14 +6,22 @@
 // }).listen(8080);
 
 
-const http = require('http');
- const server=http.createServer(function (res, req) {
-    req.write("<b>hello</b><br>");
-    req.write("<span style= 'color:green';> user~~~</span>");
-    req.end();
+// const http = require('http');
+//  const server=http.createServer(function (res, req) {
+//     req.write("<b>hello</b><br>");
+//     req.write("<span style= 'color:green';> user~~~</span>");
+//     req.end();
+// });
+// const port=5000;
+// server.listen(port,()=>{
+//     console.log(`server is runnimg on http://localhost:${port}`);
+// })
+
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    res.write("<b>hello</b>");
 });
 const port=5000;
 server.listen(port,()=>{
-    console.log(`server is runnimg on http://localhost:${port}`);
+    console.log(`server is running on http://localhost:${port}`);
 })
-
