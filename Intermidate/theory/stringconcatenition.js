ddde924078916da0fd669400ab7388c8308d1afc
Mahak <sainimahak004@gmail.com>
2023-10-25 19:28:32 +0530

    Update stringconcatenition.js

--- Intermidate/theory/stringconcatenition.js
+++ Intermidate/theory/stringconcatenition.js
@@ -13,3 +13,45 @@ alert('1' + 2 + 2); // "122" and not "14"
 
 alert( 6 - '2' ); // 4, converts '2' to a number
 alert( '6' / '2' ); // 3, converts both operands to numbers
+
+
+The plus + exists in two forms: the binary form that we used above and the unary form.
+
+The unary plus or, in other words, the plus operator + applied to a single value, doesn’t do anything to numbers. But if the operand is not a number, the unary plus converts it into a number.
+
+For example:
+
+// No effect on numbers
+let x = 1;
+alert( +x ); // 1
+
+let y = -2;
+alert( +y ); // -2
+
+// Converts non-numbers
+alert( +true ); // 1
+alert( +"" );   // 0
+// It actually does the same thing as Number(...), but is shorter.
+
+let apples = "2";
+let oranges = "3";
+
+alert( apples + oranges ); // "23", the binary plus concatenates strings
+// If we want to treat them as numbers, we need to convert and then sum them:
+
+let apple = "2";
+let orange = "3";
+
+// both values converted to numbers before the binary plus
+alert( +apple + +orange ); // 5
+
+// the longer variant
+// alert( Number(apples) + Number(oranges) ); // 5
+
+let a = 1;
+let b = 2;
+
+let c = 3 - (a = b + 1);
+
+alert( a ); // 3
+alert( c ); // 0
