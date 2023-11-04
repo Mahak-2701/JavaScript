67766d8b8e81579e0d54678b199c48574a66253c
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-11-04 18:33:28 +0530

    ~~

--- Express/route.js
+++ Express/route.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// Define a route that responds with a message
+app.get('/greet', (req, res) => {
+    res.send('Hello from the /greet route!');
+});
+
+// Define another route
+app.get('/about', (req, res) => {
+    res.send('This is the about page.');
+});
