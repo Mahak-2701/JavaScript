1b14dce0bcf597ed9454214c7ed0ddf9a80fa2b2
Mahak <sainimahak004@gmail.com>
2023-11-09 11:35:49 +0530

    Update powfun.js

--- Intermidate/theory/powfun.js
+++ Intermidate/theory/powfun.js
@@ -1,3 +1,22 @@
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x 
 // by itself n times and returns the result.
 
+
+function pow(x, n) {
+    let result = x;
+  
+    for (let i = 1; i < n; i++) {
+      result *= x;
+    }
+  
+    return result;
+  }
+  
+  let x = prompt("x?", '');
+  let n = prompt("n?", '');
+  
+  if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`);
+  } else {
+    alert( pow(x, n) );
+  }
