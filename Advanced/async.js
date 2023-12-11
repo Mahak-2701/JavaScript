f1eb85705d6ddde53a866df81514a8547681bfc9
Mahak <131138313+Mahak-2701@users.noreply.github.com>
2023-12-11 01:30:58 -1200

    *

--- Advanced/async.js
+++ Advanced/async.js
@@ -1,6 +1,7 @@
 var array = new Array([]);
 
 async function fetchData(arr) {
+    await this.arr
     return new Promise((resolve, reject) => {
         arr.length > 0 ? resolve(arr) : reject(new Error("Array is empty lol "));
     })
